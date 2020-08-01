8d2e9ce68ee46f34004c3b4078554a68ef3071e5
gb2392 <gb2392>
2020-08-01 08:24:18 -0700

    Clean up.

--- AudioCapture/STM32F302R8/source/main.c
+++ AudioCapture/STM32F302R8/source/main.c
@@ -3,11 +3,23 @@
 #include "debug.h"
 #include "time.h"
 #include "serial.h"
+#include <stdint.h>
 
+/*
+ *main - calls initialization routines and implements an infinite loop. 
+ *
+ *This function calls routines to configure:
+ *      -the system clock source and peripheral bus frequencies
+ *      -the SysTick hardware to produce interrupts on a 1 ms basis 
+ *      -the USART 1 peripheral to be used for data transmission 
+ *
+ *Otherwise, it is being used for tests and development. 
+ */
 void main(void)
 {
-        uint8_t test_vector[] = {0xab, 0xcd, 0xef, 0xde, 0xad, 0xbe, 0xef};
-        
+        const uint32_t COUNT_MAX = 250;
+        uint8_t test_vector[] = {0xab, 0xcd, 0xef, 0xde, 0xad, 0xbe, 0xef, 0xcf, 0x1A , 0x32};
+        uint32_t count = 0;
 
         clock_setup();
         debug_setup();
@@ -18,7 +30,13 @@ void main(void)
         {      
                 if(is_usart_1_ready())
                 {
-                        usart_1_transmit((void *)test_vector, 7);
+                        usart_1_transmit((void *)test_vector, 10);
+                        count++;
+                }
+
+                if(count == COUNT_MAX)
+                {
+                        break;
                 }
         }
 }
